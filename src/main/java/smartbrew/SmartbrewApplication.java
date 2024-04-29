7435dc035835df7e0a8d2230be502a149070291c
poeo1877 <brin7584@gmail.com>
2024-04-29 16:37:19 +0900

    [init] Back-end init back-end using spring initialier

---
+++ src/main/java/smartbrew/SmartbrewApplication.java
@@ -0,0 +1,13 @@
+package smartbrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbrewApplication.class, args);
+	}
+
+}
