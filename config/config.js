ea0a9c586f2e34a42542fbd67e0a9fdc1a986e6b
poeo1877 <brin7584@gmail.com>
2024-09-22 15:50:47 +0900

    Change config.json to config.js .env를 사용해 db 연결 정보 사용 이전에 merge 과정에서 잘 진행되지 않았던 부분 수정

---
+++ config/config.js
@@ -0,0 +1,28 @@
+const development = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
+};
+
+const production = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
+};
+
+const test = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
+};
+
+module.exports = { development, production, test };
